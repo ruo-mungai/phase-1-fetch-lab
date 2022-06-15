1278f61df06b92871ecd496c71904aa7c71b4046
Patrick <ruomungai@gmail.com>
2022-06-15 07:51:41 +0300

    commit tree 33b6885f7ecfa09515d34f3897687f919316526a

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
+    h2.innerHTML = books.name;
     main.appendChild(h2);
   });
 }
